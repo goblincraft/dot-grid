e1bea5170eb3a26d194eb9ce06c29c2ab8b35cec
goblincraft <goblin.software.co@gmail.com>
2025-07-27 12:24:51 -0400

    feature / create dot grid demo config controls and listen for color change

--- demo/main.js
+++ demo/main.js
@@ -2,8 +2,31 @@ import { DotGrid } from '../dist/module.js';
 
 document.addEventListener("DOMContentLoaded", () => {
 
+    const elements = {
+        color: "dot-color-picker-control",
+        size: "dot-size-input-control"
+    }
+    let options = {
+        size: 5,
+        color: "#00000",
+        margin: 5
+    }
+
+    function listenForColorInputChanges() {
+        document.getElementById(elements.color).addEventListener("change", (e) => {
+            options.color = e.target.value;
+        });
+    }
+
+    function setDefaults() {
+        document.getElementById(elements.color).value = options.color;
+        document.getElementById(elements.size).value = options.size;
+    }
+
     function init() {
+        setDefaults();
         DotGrid.draw("dot-grid");
+        listenForColorInputChanges();
     }
 
     init();
